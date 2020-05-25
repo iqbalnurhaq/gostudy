45cf4938bae5ed9f87313a2e8ce0717a4f80ac60
unknown <iqbalnur833@gmail.com>
2020-05-25 16:43:54 +0700

    daftar perubahan

--- application/controllers/guru_usr_clx/G_materi.php
+++ application/controllers/guru_usr_clx/G_materi.php
@@ -20,8 +20,37 @@ class G_materi extends CI_Controller {
 	}
 	
 	public function tambah_materi(){
-		
+		$config['upload_path']          = './file_materi/';
+	    $config['allowed_types']        = 'pdf|doc|docx|ppt';
+    	$config['max_size']             = 50000;
+	
+		$this->load->library('upload', $config);
+	    $this->upload->initialize($config);
+	
+		if ( ! $this->upload->do_upload('file_materi'))
+		{
+				$this->session->set_flashdata('message',  $this->upload->display_errors());
+				redirect('guru_usr_clx/G_materi');
+		}else{
+				$data = array('upload_data' => $this->upload->data());
+				$this->save_materi($data);
+				$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Materi berhasil ditambahkan</div>');
+				redirect('guru_usr_clx/G_materi');
+		}
 	}
+
+	function save_materi($upload){
+		$data = array(
+		  'nama_materi' => $this->input->post('namaMateri'),
+		  'nama_file' => $upload['upload_data']['file_name'],
+		  'ukuran_file' => $upload['upload_data']['file_size'],
+		  'tipe_file' => $upload['upload_data']['file_type'],
+		  'kode_mapel' => $this->session->userdata('kode_mapel'),
+		  'kode_kls' => $this->input->post('idKls')
+		);
+		$id = $this->session->userdata('id');
+		$this->M_g_materi->uploadFileMateri($data);
+	  }
 
 
 
